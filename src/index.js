06521745fe6a15d0a9f272743cbdec8f6b52ca3f
Max Datsenko <datsenko.md@gmail.com>
2023-11-08 12:22:02 +0200

    remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const inputClick = () => console.log('Clicked');
+const mouseOver = () => console.log('Mouse Over');
+
+const helpText = 'Hello text!';
+
+const elements = (
+  <div className="name">
+    <h1>{helpText}</h1>
+    <input placeholder={helpText}
+      onClick={inputClick} onMouseOver={mouseOver}/>
+    <p>{helpText === 'Hello text!' ? 'Yes' : 'No'}</p>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = document.getElementById('app');
+
+ReactDOM.render(elements, app);
